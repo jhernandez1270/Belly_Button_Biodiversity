e8688b7e8ff36f932faebc5ea7a2679795f53313
Jesse Hernandez <jessehernandez@Jesses-Home-MBPro.local>
2022-06-30 06:54:44 -0700

    Module 12 Deliverable 4 fixed barLayout error

--- js/charts.js
+++ js/charts.js
@@ -156,11 +156,13 @@ function buildCharts(sample) {
 
     var barData = [trace1];
 
-    // // 9. Create the layout for the bar chart. 
+
+
+    // // 9. Create the layout for the bar chart.
     var barLayout = {
       title: "Top Ten Bacteria Cultures Found"
-      paper_bgcolor: "#ffffff60",
     };
+
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
 
@@ -188,15 +190,14 @@ function buildCharts(sample) {
     var bubbleLayout = {
       title: "Bacteria Cultures per Sample",
       xaxis: {title: "OTU IDs"},
-      hovermode: 'closest',
-      paper_bgcolor: "rgba(255,255,255,0.6)",
-      margin: {
-        l: 50,
-        r: 50,
-        b: 100,
-        t: 100,
-        pad: 4
-      }
+      hovermode: 'closest'
+      // margin: {
+      //   l: 50,
+      //   r: 50,
+      //   b: 100,
+      //   t: 100,
+      //   pad: 4
+      // }
     };
 
     // 3. Use Plotly to plot the data with the layout.
@@ -237,7 +238,6 @@ function buildCharts(sample) {
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
       width: 600, height: 500, margin: { t: 0, b: 0 }
-      paper_bgcolor: "rgba(255,255,255,0.5)",
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
